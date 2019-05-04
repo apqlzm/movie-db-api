14613df29da7c33e1dec469eae53503f820ab9d8
apqlzm <artur-czyzewski@protonmail.com>
2019-05-04 22:48:30 +0200

    add get movies test

--- omdbapi/tests.py
+++ omdbapi/tests.py
@@ -1,3 +1,56 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from omdbapi import models
+import json
 
-# Create your tests here.
+
+class MovieTests(APITestCase):
+    def setUp(self):
+        models.Movie.objects.create(title='Some title', imdbid='123asd')
+
+    def test_list_movies(self):
+        """ tests GET /movies """
+
+        url = reverse('movies')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            json.loads(response.content), [
+                {'id': 1,
+                 'title': 'Some title',
+                 'year': '',
+                 'rated': '',
+                 'released': None,
+                 'runtime': None,
+                 'genre': '',
+                 'director': '',
+                 'writer': '',
+                 'actors': '',
+                 'plot': '',
+                 'language': '',
+                 'country': '',
+                 'awards': '',
+                 'poster': '',
+                 'metascore': None,
+                 'imdbrating': None,
+                 'imdbvotes': None,
+                 'imdbid': '123asd',
+                 'type_picture': '',
+                 'dvd': None,
+                 'boxoffice': '',
+                 'production': '',
+                 'website': '',
+                 'totalseasons': None,
+                 'ratings': []}
+            ]
+        )
+
+    # def test_create_movie(self):
+    #     """ tests POST /movies """
+
+    #     url = reverse('movies')
+    #     data = {'title': 'Django'}
+    #     response = self.client.post(url, data, format='json')
+    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    #     self.assertEqual(Movie.objects.count(), 1)
--- omdbapi/urls.py
+++ omdbapi/urls.py
@@ -3,9 +3,8 @@ from django.urls import path
 from omdbapi import views
 
 urlpatterns = [
-    path('movies/', views.movies, name='api_find_trains'),
+    path('movies/', views.movies, name='movies'),
     path('movies/<int:movie_id>/', views.UpdateDeleteMovieView.as_view()),
     path('comments/', views.CommentView.as_view()),
     path('top/', views.top),
-
 ]
