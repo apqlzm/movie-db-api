e4b494d6f653aa133d58163b8180355a13898966
apqlzm <artur-czyzewski@protonmail.com>
2019-05-04 23:55:46 +0200

    update production settings

--- decathlon_movies_api/settings_production.py
+++ decathlon_movies_api/settings_production.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ['DJANGO_DEBUG'])
+DEBUG = bool(int(os.environ['DJANGO_DEBUG']))
 
 ALLOWED_HOSTS = ["movie-db-api.cleverapps.io"]
 
